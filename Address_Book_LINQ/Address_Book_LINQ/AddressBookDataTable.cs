cc91d4b94297801e48b68fb2911d30c46e4a687a
amit <[amirana14325@gmail.com]>
2021-04-20 00:00:41 +0530

    [Amit Rana]Added::create data table object

---
+++ Address_Book_LINQ/Address_Book_LINQ/AddressBookDataTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Address_Book_LINQ
+{
+    public class AddressBookDataTable
+    {
+        DataTable addressBookDataTable = new DataTable();
+    }
+}
