9ca4b0a5af12972dc8fa74c6b4f2dcd84ffd8c98
amit <[amirana14325@gmail.com]>
2021-04-20 00:05:52 +0530

    [Amit Rana]Added::add fields on Data table

--- Address_Book_LINQ/Address_Book_LINQ/AddressBookDataTable.cs
+++ Address_Book_LINQ/Address_Book_LINQ/AddressBookDataTable.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Text;
+using System.Linq;
 
 namespace Address_Book_LINQ
 {
     public class AddressBookDataTable
     {
-        DataTable addressBookDataTable = new DataTable();
+        public void CreateAddressBookDataTable()
+        {
+            DataTable addressBookDataTable = new DataTable();
+
+            addressBookDataTable.Columns.Add("FirstName", typeof(string));
+            addressBookDataTable.Columns.Add("LastName", typeof(string));
+            addressBookDataTable.Columns.Add("Address", typeof(string));
+            addressBookDataTable.Columns.Add("City", typeof(string));
+            addressBookDataTable.Columns.Add("State", typeof(string));
+            addressBookDataTable.Columns.Add("Zip", typeof(int));
+            addressBookDataTable.Columns.Add("PhoneNumber", typeof(long));
+            addressBookDataTable.Columns.Add("Email", typeof(string));
+        }
     }
 }
