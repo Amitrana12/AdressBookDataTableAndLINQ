7589192b621ec7b67886bc6b73d099a07797b973
amit <[amirana14325@gmail.com]>
2021-04-19 23:56:33 +0530

    [Amit Rana]Added::welcome massege

---
+++ Address_Book_LINQ/Address_Book_LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Address_Book_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcomr to my Addresh book using LINQand DATATable by Amit Rana [- _-]");
+        }
+    }
+}
